888312cfb70f4452a3c9883694fc9855e74d82a7
jeeejeeeornah <jeeejeeeornah@gmail.com>
2026-05-28 21:47:28 +0200

    Update scan.js

--- api/scan.js
+++ api/scan.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { imageUrl } = req.body;
+   const { imageUrl } = req.body;
 
     if (!imageUrl) {
       return res.status(400).json({ error: 'No image URL provided' });
